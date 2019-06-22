ef7d3804d36741b26e67f9359d4e877bf4cfeeb0
Evgenii Dergachev <edergachev@bk.ru>
2019-06-22 17:23:13 +1000

    adding some smoke testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
+import NavBar from './NavBar';
+import Input from './Input';
+import Output from './Output';
+import colors from './colors.js';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const cuisines = {General: true, Vegetarian: true, American: true, French: true, Indian: true, Italian: true, Jewish: true};
+
+it('renders navbar properly', () => {
+  const wrapper = shallow(<NavBar cuisines={cuisines} scheme={colors["lavender"]} />);
+  const instruction = <div className="msg instruction-msg">Click to select cuisines</div>;
+  expect(wrapper.contains(instruction)).toEqual(true);
+});
+
+it('renders input properly', () => {
+  const wrapper = shallow(<Input cuisines={cuisines} scheme={colors["lavender"]} />);
+  expect(wrapper.exists('input')).toEqual(true);
+});
+
+it('renders output properly', () => {
+  const wrapper = shallow(<Output dish={[]} scheme={colors["lavender"]} />);
+  const title = <h1 id="app-title">JS Kitty Cooking</h1>;
+  expect(wrapper.contains(title)).toEqual(true);
 });
